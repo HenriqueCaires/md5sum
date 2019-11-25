ec9b9e00dae242cad601d1e89e668faab49c55d9
Henrique Farias Caires Clemente <henrique@caires.net.br>
2019-11-25 12:30:01 -0300

    add code to repo

---
+++ README.md
@@ -0,0 +1,6 @@
+# md5sum
+Print MD5 checksums
+
+publish:
+
+    dotnet publish -r win-x64 -c Release /p:PublishSingleFile=true
---
+++ md5sum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace md5sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var file in args)
+                if (File.Exists(file))
+                    Console.WriteLine($"{CalculateMD5(file)}  {file}");
+                else
+                    Console.WriteLine($"md5sum: {file}: No such file or directory");
+        }
+
+        public static string CalculateMD5(string file)
+        {
+            using var md5 = MD5.Create();
+            using var stream = File.OpenRead(file);
+            return
+                string.Join(string.Empty,
+                    md5.ComputeHash(stream)
+                        .Select(b => b.ToString("x2"))
+                )
+                ;
+        }
+    }
+}
